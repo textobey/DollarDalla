9ef53e71c76d291163ee445a946e65fb6087aa05
textobey <dogsan_e@naver.com>
2024-03-26 16:21:54 +0900

    [ADD] 프로젝트 생성

---
+++ DollarDalla/DollarDallaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DollarDallaApp.swift
+//  DollarDalla
+//
+//  Created by 이서준 on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct DollarDallaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
